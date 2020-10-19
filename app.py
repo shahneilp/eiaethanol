a48a6f42f3ccff2ebcca3ff96148fe08ea388908
Neil Shah <NSHAH0617@GMAIL.COM>
2020-10-18 20:47:28 -0400

    Fixed Central Time zone

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ import os
 import pandas as pd
 from flask import Flask
 from datetime import datetime, date
+import pytz
 
-timestamp = datetime.now().strftime('%m/%d/%Y-%H:%M:%S')
+timestamp = datetime.now(tz=pytz.timezone('US/Central')).strftime('%m/%d/%Y-%H:%M:%S')
 
 # Overall Stats
 blendurl = 'https://api.grainstats.com/eia/blended_ethanol.json'
@@ -99,7 +100,7 @@ def eia():
     update = '<br>' + 'Last Updated: ' + last_updated + '<br>'
     foot = 'Powered by: <a href="https://twitter.com/GrainStats">GrainStats</a> and Made by: <a ' \
            'href="https://twitter.com/shah_neil">Neil Shah</a> '
-    html = title + '<center>' + style + body + update + foot + '<br>' + timestamp + '</center>'
+    html = title + '<center>' + style + body + update + foot + '<br>' + 'Loaded '+ timestamp + '</center>'
     return html
 
 
