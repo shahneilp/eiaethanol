cb5ea3234cc342cc13a7c119f022dfb68cbcb514
Neil Shah <NSHAH0617@GMAIL.COM>
2020-12-08 17:13:21 -0500

    Put JSON call in flask main file

--- app.py
+++ app.py
@@ -11,81 +11,6 @@ import pandas as pd
 import pytz
 from flask import Flask
 
-timestamp = datetime.now(tz=pytz.timezone('US/Central')).strftime('%m/%d/%Y-%H:%M:%S')
-
-# Overall Stats
-blendurl = 'https://api.grainstats.com/eia/blended_ethanol.json'
-importurl = 'https://api.grainstats.com/eia/ethanol_imports.json'
-produrl = 'https://api.grainstats.com/eia/ethanol_production.json'
-stocksurl = 'https://api.grainstats.com/eia/ethanol_stocks.json'
-gasurl = 'https://api.grainstats.com/eia/gasoline_demand.json'
-
-prod = pd.read_json(produrl, orient='index')
-stocks = pd.read_json(stocksurl, orient='index')
-blend = pd.read_json(blendurl, orient='index')
-gas = pd.read_json(gasurl, orient='index')
-imports = pd.read_json(importurl, orient='index')
-
-survey = prod.loc['survey date'][0]
-last_updated = str(prod.loc['last updated'][0])
-
-col_names = ['Production', 'Stocks', 'Blend', 'Gasoline', 'Imports']
-df_list = [prod, stocks, blend, gas, imports]
-df = pd.concat(df_list, axis=1)
-df.columns = col_names
-df.drop(['country', 'uom', 'region', 'statistic', 'last_week', 'location', 'last updated'], axis=0, inplace=True)
-df.reindex(['value', 'week_change'])
-df = df.reindex(['value', 'week_change'])
-# Production Granular
-
-padd1produrl = 'https://api.grainstats.com/eia/ethanol_production_padd_1.json'
-padd2produrl = 'https://api.grainstats.com/eia/ethanol_production_padd_2.json'
-padd3produrl = 'https://api.grainstats.com/eia/ethanol_production_padd_3.json'
-padd4produrl = 'https://api.grainstats.com/eia/ethanol_production_padd_4.json'
-padd5produrl = 'https://api.grainstats.com/eia/ethanol_production_padd_5.json'
-
-padd1prod = pd.read_json(padd1produrl, orient='index')
-padd2prod = pd.read_json(padd2produrl, orient='index')
-padd3prod = pd.read_json(padd3produrl, orient='index')
-padd4prod = pd.read_json(padd4produrl, orient='index')
-padd5prod = pd.read_json(padd5produrl, orient='index')
-
-prod_names = ['Total', 'PADD1', 'PADD2', 'PADD3', 'PADD4', 'PADD5']
-
-df_list = [prod, padd1prod, padd2prod, padd3prod, padd4prod, padd5prod]
-proddf = pd.concat(df_list, axis=1)
-proddf.columns = prod_names
-proddf.drop(['country', 'uom', 'region', 'statistic', 'last_week', 'location', 'last updated'], axis=0, inplace=True)
-proddf.reindex(['value', 'week_change'])
-proddf = proddf.reindex(['value', 'week_change'])
-
-# Stocks
-
-
-padd1stocksurl = 'https://api.grainstats.com/eia/ethanol_stocks_padd_1.json'
-padd2stocksurl = 'https://api.grainstats.com/eia/ethanol_stocks_padd_2.json'
-padd3stocksurl = 'https://api.grainstats.com/eia/ethanol_stocks_padd_3.json'
-padd4stocksurl = 'https://api.grainstats.com/eia/ethanol_stocks_padd_4.json'
-padd5stocksurl = 'https://api.grainstats.com/eia/ethanol_stocks_padd_5.json'
-
-padd1stocks = pd.read_json(padd1stocksurl, orient='index')
-padd2stocks = pd.read_json(padd2stocksurl, orient='index')
-padd3stocks = pd.read_json(padd3stocksurl, orient='index')
-padd4stocks = pd.read_json(padd4stocksurl, orient='index')
-padd5stocks = pd.read_json(padd5stocksurl, orient='index')
-
-stocks_names = ['Total', 'PADD1', 'PADD2', 'PADD3', 'PADD4', 'PADD5']
-
-df_list = [stocks, padd1stocks, padd2stocks, padd3stocks, padd4stocks, padd5stocks]
-stocksdf = pd.concat(df_list, axis=1)
-stocksdf.columns = stocks_names
-stocksdf.drop(['country', 'uom', 'region', 'statistic', 'last_week', 'location', 'last updated'], axis=0, inplace=True)
-stocksdf.reindex(['value', 'week_change'])
-stocksdf = stocksdf.reindex(['value', 'week_change'])
-
-# print(df.transpose())
-# print(proddf.transpose())
-# print(stocksdf.transpose())
 
 
 app = Flask(__name__)
@@ -93,6 +18,79 @@ app = Flask(__name__)
 
 @app.route("/")
 def eia():
+    timestamp = datetime.now(tz=pytz.timezone('US/Central')).strftime('%m/%d/%Y-%H:%M:%S')
+
+    # Overall Stats
+    blendurl = 'https://api.grainstats.com/eia/blended_ethanol.json'
+    importurl = 'https://api.grainstats.com/eia/ethanol_imports.json'
+    produrl = 'https://api.grainstats.com/eia/ethanol_production.json'
+    stocksurl = 'https://api.grainstats.com/eia/ethanol_stocks.json'
+    gasurl = 'https://api.grainstats.com/eia/gasoline_demand.json'
+
+    prod = pd.read_json(produrl, orient='index')
+    stocks = pd.read_json(stocksurl, orient='index')
+    blend = pd.read_json(blendurl, orient='index')
+    gas = pd.read_json(gasurl, orient='index')
+    imports = pd.read_json(importurl, orient='index')
+
+    survey = prod.loc['survey date'][0]
+    last_updated = str(prod.loc['last updated'][0])
+
+    col_names = ['Production', 'Stocks', 'Blend', 'Gasoline', 'Imports']
+    df_list = [prod, stocks, blend, gas, imports]
+    df = pd.concat(df_list, axis=1)
+    df.columns = col_names
+    df.drop(['country', 'uom', 'region', 'statistic', 'last_week', 'location', 'last updated'], axis=0, inplace=True)
+    df.reindex(['value', 'week_change'])
+    df = df.reindex(['value', 'week_change'])
+    # Production Granular
+
+    padd1produrl = 'https://api.grainstats.com/eia/ethanol_production_padd_1.json'
+    padd2produrl = 'https://api.grainstats.com/eia/ethanol_production_padd_2.json'
+    padd3produrl = 'https://api.grainstats.com/eia/ethanol_production_padd_3.json'
+    padd4produrl = 'https://api.grainstats.com/eia/ethanol_production_padd_4.json'
+    padd5produrl = 'https://api.grainstats.com/eia/ethanol_production_padd_5.json'
+
+    padd1prod = pd.read_json(padd1produrl, orient='index')
+    padd2prod = pd.read_json(padd2produrl, orient='index')
+    padd3prod = pd.read_json(padd3produrl, orient='index')
+    padd4prod = pd.read_json(padd4produrl, orient='index')
+    padd5prod = pd.read_json(padd5produrl, orient='index')
+
+    prod_names = ['Total', 'PADD1', 'PADD2', 'PADD3', 'PADD4', 'PADD5']
+
+    df_list = [prod, padd1prod, padd2prod, padd3prod, padd4prod, padd5prod]
+    proddf = pd.concat(df_list, axis=1)
+    proddf.columns = prod_names
+    proddf.drop(['country', 'uom', 'region', 'statistic', 'last_week', 'location', 'last updated'], axis=0,
+                inplace=True)
+    proddf.reindex(['value', 'week_change'])
+    proddf = proddf.reindex(['value', 'week_change'])
+
+    # Stocks
+
+    padd1stocksurl = 'https://api.grainstats.com/eia/ethanol_stocks_padd_1.json'
+    padd2stocksurl = 'https://api.grainstats.com/eia/ethanol_stocks_padd_2.json'
+    padd3stocksurl = 'https://api.grainstats.com/eia/ethanol_stocks_padd_3.json'
+    padd4stocksurl = 'https://api.grainstats.com/eia/ethanol_stocks_padd_4.json'
+    padd5stocksurl = 'https://api.grainstats.com/eia/ethanol_stocks_padd_5.json'
+
+    padd1stocks = pd.read_json(padd1stocksurl, orient='index')
+    padd2stocks = pd.read_json(padd2stocksurl, orient='index')
+    padd3stocks = pd.read_json(padd3stocksurl, orient='index')
+    padd4stocks = pd.read_json(padd4stocksurl, orient='index')
+    padd5stocks = pd.read_json(padd5stocksurl, orient='index')
+
+    stocks_names = ['Total', 'PADD1', 'PADD2', 'PADD3', 'PADD4', 'PADD5']
+
+    df_list = [stocks, padd1stocks, padd2stocks, padd3stocks, padd4stocks, padd5stocks]
+    stocksdf = pd.concat(df_list, axis=1)
+    stocksdf.columns = stocks_names
+    stocksdf.drop(['country', 'uom', 'region', 'statistic', 'last_week', 'location', 'last updated'], axis=0,
+                  inplace=True)
+    stocksdf.reindex(['value', 'week_change'])
+    stocksdf = stocksdf.reindex(['value', 'week_change'])
+
     overall = df.transpose().to_html(justify='center')
     production = proddf.transpose().to_html()
     stocks = stocksdf.transpose().to_html()
